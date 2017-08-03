171a82709ca6d93b1d6c4c623cd8be6279bb4cb1
Velreine <nicky-g-hansen@hotmail.com>
2017-08-03 12:42:19 +0200

    Second commit

---
+++ Dupfinder/MySQL_DB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Dupfinder
+{
+    class MySQL_DB
+    {
+
+       // Server info::
+       //host: 10.106.169.40
+       //database: HashDB
+       //user: outside
+       //password: b36eWa
+ 
+
+        
+        private MySqlConnection GetConnection(string server, string database, string uid, string password)
+        {
+            
+            string s_conn = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            MySqlConnection conn = new MySqlConnection(s_conn);
+
+
+            return conn;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            string s_conn = "SERVER=" + "10.106.169.40" + ";" + "DATABASE=" +
+            "hashdb" + ";" + "UID=" + "outside" + ";" + "PASSWORD=" + "b36eWa" + ";";
+
+            MySqlConnection conn = new MySqlConnection(s_conn);
+
+            return conn;
+
+
+        }
+
+        private void OpenConnection(MySqlConnection conn)
+        {
+            conn.Open();
+
+        }
+
+        private void ExecQuery(string query, MySqlConnection conn)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            cmd.ExecuteScalar();
+
+        }
+
+
+
+
+    }
+}
--- Dupfinder/Program.cs
+++ Dupfinder/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Dupfinder
 {
@@ -10,6 +11,33 @@ namespace Dupfinder
     {
         static void Main(string[] args)
         {
+
+            MySqlConnection conn;
+            string server = "10.106.169.40";
+            string database = "HashDB";
+            string uid = "outside";
+            string password = "b36eWa";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            conn = new MySqlConnection(connectionString);
+
+            // Open the connection.
+            conn.Open();
+
+            // Execute some SQL code:
+
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO all_files (Path, Size, hash_MD5, hash_SHA256) VALUES('C:\\\\fedpik\\\\pik.exe', 20, 'MD5HASHSAMPLE', 'SHA256HASHSAMPLE'); ", conn);
+
+            cmd.ExecuteScalar();
+
+
         }
+
+
+
+
+
     }
 }
