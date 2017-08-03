7d3a84609dffef47ed254eda8e8d3dbe46778258
Velreine <Velreine@VELREINE-ASUS>
2017-08-03 10:37:17 +0200

    Add project files.

---
+++ Dupfinder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dupfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
